43b9add328dcac2f35ca10c9128331c39893017d
Coencidental <cdrexler1999@gmail.com>
2019-09-17 14:18:41 +1000

    Updates

--- Scripts/scrollupdate.js
+++ Scripts/scrollupdate.js
@@ -2,7 +2,7 @@ function resizeHeaderOnScroll() {
   
   let distanceY = mainContainer.scrollTop;
 
-  const percentage = clamp(distanceY / 500.0, 0, 3);
+  const percentage = clamp(distanceY / 500.0, 0, 1);
   document.getElementsByClassName("MainContainer")[0].style.setProperty('--parallaxopacity', clamp((0.01/percentage*10), 0, 1));
   
 
